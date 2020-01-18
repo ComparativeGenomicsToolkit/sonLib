c4ecdd09908babf4e5b5aa72ed4b30d67478e839
Mark Diekhans <markd@ucsc.edu>
2020-01-17 19:00:37 -0800

    reverted compile flags

--- include.mk
+++ include.mk
@@ -54,10 +54,10 @@ ifndef CXX
   else ifeq ($(SYS),Darwin) #This is to deal with the Mavericks replacing gcc with clang fully
     cpp = clang++
   else
-    cpp = g++ -std=c++0x -fpermissive
+    cpp = g++
   endif
 else
-  cpp = ${CXX} -std=c++0x -fpermissive
+  cpp = ${CXX}
 endif
 
 # -Wno-unused-result
