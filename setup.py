e25181bed32c03c783f96c411c2b2773a9fd72a9
Mark Diekhans <markd@ucsc.edu>
2020-01-27 13:06:39 -0800

    uped version for python

--- setup.py
+++ setup.py
@@ -8,23 +8,21 @@ class BuildWithMake(distutils.command.build_py.build_py):
     """
     Build using make.
     Then do the default build logic.
-    
+
     """
     def run(self):
         # Call make.
         subprocess.check_call(["make"])
-        
+
         # Keep building the Python stuff
         distutils.command.build_py.build_py.run(self)
 
 
 setup(name="sonLib",
-      version="1.0",
+      version="2.0",
       description="Small general purpose library for C and Python with focus on "
       "bioinformatics.",
       author="Benedict Paten",
       author_email="benedict@soe.ucsc.edu",
       package_dir = {'': 'src'},
       packages=find_packages(where='src'))
-
-    
