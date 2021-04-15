c5e585bd8a7e915b58f42225adaf3fa5a2af3314
Glenn Hickey <glenn.hickey@gmail.com>
2021-04-15 13:16:31 -0400

    Revert "safer getTempFile"
    This reverts commit ba7917747fe6ae0a185398864d6b59df05ca8af8.

--- C/impl/commonC.c
+++ C/impl/commonC.c
@@ -20,7 +20,6 @@
 #include <unistd.h> // for "legacy" systems
 #include <fcntl.h>
 #include <errno.h>
-#include <stdlib.h>
 
 void exitOnFailure(int64_t exitValue, const char *failureMessage, ...) {
     if (exitValue != 0) {
@@ -866,13 +865,22 @@ char *getTempFile(void) {
         tmpdir = "/tmp";
     }
 
-    char* template = malloc((strlen(tmpdir) + 12) * sizeof(char));
-    if (tmpdir[strlen(tmpdir)-1] == '/') {
-        sprintf(template, "%sstTmpXXXXXX", tmpdir);
-    } else {
-        sprintf(template, "%s/stTmpXXXXXX", tmpdir);
+    // todo: fix this crap
+    static int64_t counter = 0;
+    while(counter < INT64_MAX) {
+        char *pattern = stString_print(tmpdir[strlen(tmpdir)-1] == '/' ? "%sstTmp%" PRIi64 "_%" PRIi64 "" : "%s/stTmp%" PRIi64 "_%" PRIi64 "", tmpdir, getpid(), counter++);
+        int fd = open(pattern, O_CREAT|O_EXCL, 0600);
+        if(fd >= 0) {
+            close(fd);
+            return pattern;
+        }
+        else if(errno != EEXIST){
+            st_errnoAbort("Couldn't create temporary file's file descriptor for %s", pattern);
+        }
+        free(pattern);
     }
-    return mktemp(template);
+    st_errnoAbort("Exhausted temporary file patterns"); //This is pretty bloody unlikely
+    return NULL;
 }
 
 void removeTempFile(char *tempFile) {
