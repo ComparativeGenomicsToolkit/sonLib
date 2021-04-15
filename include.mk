e3e62163266f4d220b39cde563374acd299df229
Glenn Hickey <glenn.hickey@gmail.com>
2021-04-15 13:05:37 -0400

    set gnu99

--- include.mk
+++ include.mk
@@ -12,7 +12,7 @@ LIBDIR ?= ${rootPath}/../sonLib/lib
 #
 # - enable PIC as some newer GCC versions are configured to link PIE by default,
 #   which will not link with non-PIC objcts
-CFLAGS += -fPIC
+CFLAGS += -fPIC -std=gnu99
 CXXFLAGS += -fPIC
 
 ##
